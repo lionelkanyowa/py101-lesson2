89add0fdc47041cca61deec6aba5c29c8943b623
Lionel Kanyowa <lionelkanyowa@gmail.com>
2025-01-18 16:37:28 -0500

    chore: First commit with new project files and truthiness chapter

---
+++ truthiness.py
@@ -0,0 +1,9 @@
+#name = get_name_from_user()
+#if name:
+    #print(f"Hi {name}")
+#else:
+    #print("you must enter your name")
+
+print("hello world!")
+name = input("Enter your name: ")
+print(name)
