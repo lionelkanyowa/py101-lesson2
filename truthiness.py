4e0fc6fc9e1351145fc44e26500827bc239e4b23
Lionel Kanyowa <lionelkanyowa@gmail.com>
2025-01-27 08:48:24 -0500

    chore: started 2nd pass of lesson 2.

--- truthiness.py
+++ truthiness.py
@@ -1,9 +1,14 @@
-#name = get_name_from_user()
-#if name:
-    #print(f"Hi {name}")
-#else:
-    #print("you must enter your name")
+def get_name_from_user():
+   return input("Please enter your name: ")
 
-print("hello world!")
-name = input("Enter your name: ")
-print(name)
+name = get_name_from_user()
+if name:
+    print(f"Hi {name}")
+else:
+    print("You must enter your name")
+
+
+
+# print("hello world!")
+# name = input("Enter your name: ")
+# print(name)
