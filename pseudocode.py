7c3a966c8d5a4807f0f6ae7ff89633ff202768eb
Lionel Kanyowa <lionelkanyowa@gmail.com>
2025-01-29 07:37:29 -0500

    chore: worked on psedocode and debugging examples.

--- pseudocode.py
+++ pseudocode.py
@@ -1,2 +1,35 @@
 # This is to review the pseudocode chapter and attempt any challenges given.
 
+# Take a look at the given pseudocode:
+
+# START
+
+# Given a collection of integers called "numbers"
+
+# ET iterator = 1
+# SET savedNumber = value within numbers collection at space 1
+
+# WHILE iterator <= length of numbers
+	# SET currentNumber = value within numbers collection at space "iterator"
+	# IF currentNumber > savedNumber
+		# savedNumber = currentNumber
+
+	# iterator = iterator + 1
+
+# PRINT savedNumber
+
+def find_greatest(numbers):
+    index = 0
+    saved_number = numbers[index]
+    
+    while index < len(numbers):
+        current_number = numbers[index]
+        if current_number > saved_number:
+            saved_number = current_number
+
+        index += 1
+
+    return saved_number
+
+
+print(find_greatest([3, 4, 5, 6, 7, 2, 10]))
